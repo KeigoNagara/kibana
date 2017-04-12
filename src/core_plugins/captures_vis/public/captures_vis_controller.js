62e919e56403be95af54322ea853141efcc98ef1
K_atc <shiftx1026@gmail.com>
2017-04-12 19:44:48 +0900

    Fix capture_vis problem: latest capture data won't be shown if elasticsearch has lots of data

--- src/core_plugins/captures_vis/public/captures_vis_controller.js
+++ src/core_plugins/captures_vis/public/captures_vis_controller.js
@@ -1,6 +1,10 @@
 import _ from 'lodash';
 import AggResponseTabifyTabifyProvider from 'ui/agg_response/tabify/tabify';
 import uiModules from 'ui/modules';
+import d3 from 'd3';
+
+const $ = require('jquery');
+
 // get the kibana/metric_vis module, and make sure that it requires the "kibana" module if it
 // didn't already
 const module = uiModules.get('kibana/captures_vis', ['kibana']);
@@ -21,36 +25,46 @@ module.controller('KbnCapturesVisController', function ($scope, $element, Privat
   }
 
   $scope.processTableGroups = function (tableGroups) {
+    const searchOption = {
+        'size': 300,
+        'sort': [
+          {'timestamp': 'desc'}
+          ],
+        'query': {
+          'match_all': {}
+        }
+      }
     $.ajax({
       // TODO: add sort timestamp by desc option
-      url: "../api/console/proxy?uri=captures/capture/_search/?size=300",
-      data: "",
-      success: function(resp){
-        var width = 1000;
-        var svg = d3.select("svg#capture_timeline").attr({width: width, height: 50});
-        var current_time = Date.now();
-        var xScale = d3.time.scale()
+      url: '../api/console/proxy?uri=captures/capture/_search/',
+      method: 'POST',
+      data: JSON.stringify(searchOption),
+      success: function (resp) {
+        const width = 1000;
+        const svg = d3.select('svg#capture_timeline').attr({ width: width, height: 50 });
+        const currentTime = Date.now();
+        const xScale = d3.time.scale()
           .domain([
-            current_time - 120 * 1000, // 開始日時
-            current_time  // 終了日時
+            currentTime - 120 * 1000, // 開始日時
+            currentTime  // 終了日時
           ])
-          .range([0, width]); 
-        var xAxis = d3.svg.axis()
+          .range([0, width]);
+        const xAxis = d3.svg.axis()
           .scale(xScale)
           .ticks(d3.time.second, 10)
-          .tickFormat(function(d,i){
-            var fmtFunc = d3.time.format("%H:%M:%S");//正規表現でどのように表示するかを
+          .tickFormat(function (d,i) {
+            const fmtFunc = d3.time.format('%H:%M:%S');//正規表現でどのように表示するかを
             return fmtFunc(d);
           });
 
-        var plotData = [];
-        var num_hits = resp.hits.hits.length;
-        console.log("num hits: %d", num_hits);
-        for(var i = 0; i < num_hits; i += 1){
-          var timestamp = resp.hits.hits[i]["_source"]["timestamp"];
-          var x = Date.parse(timestamp);
-          if(0 <= xScale(x) && xScale(x) <= width){
-            plotData.push({timestamp: timestamp, x: x, img_src: resp.hits.hits[i]["_source"]["capture"]})
+        const plotData = [];
+        const numHits = resp.hits.hits.length;
+        console.log('num hits: %d', numHits);
+        for (let i = 0; i < numHits; i += 1) {
+          const timestamp = resp.hits.hits[i]._source.timestamp;
+          const x = Date.parse(timestamp);
+          if (0 <= xScale(x) && xScale(x) <= width) {
+            plotData.push({ timestamp: timestamp, x: x, img_src: resp.hits.hits[i]._source.capture });
           }
           else {
             // console.log(resp.hits.hits[i]["_source"]["timestamp"]);
@@ -58,28 +72,34 @@ module.controller('KbnCapturesVisController', function ($scope, $element, Privat
         }
         console.log(plotData);
 
-        svg.selectAll("g").remove();
-        svg.append("g").attr({"class": "x axis"}).call(xAxis).selectAll("text").attr({dy: 10}).append("text").attr({x: 10, y: 10, "font-size": 10}).text("time");
-        svg.selectAll("circle").remove();
-        svg.selectAll("circle").data(plotData).enter().append("circle").attr({
+        svg.selectAll('g').remove();
+        svg.append('g')
+          .attr({ 'class': 'x axis' })
+          .call(xAxis)
+          .selectAll('text')
+          .attr({ dy: 10 })
+          .append('text')
+          .attr({ x: 10, y: 10, 'font-size': 10 })
+          .text('time');
+        svg.selectAll('circle').remove();
+        svg.selectAll('circle').data(plotData).enter().append('circle').attr({
           cx: function (d, i) {
-            console.log(d);
-            console.log(xScale(d.x));
+            // console.log(d);
+            // console.log(xScale(d.x));
             return xScale(d.x);
           },
           cy: 35,
           r: 5,
-          fill: "red",
-          "data-img-src": function(d) {return d.img_src},
+          fill: 'red',
+          'data-img-src': function (d) { return d.img_src; },
         })
-        .on("click", function(d) {
-          d3.select(this).attr("fill", "blue");
-          var src = d3.select(this).attr("data-img-src");
-          // FIXME: use Angler, not jQuery
-          $("#capture_image").attr("src", src); 
+        .on('click', function (d) {
+          d3.select(this).attr('fill', 'blue');
+          const src = d3.select(this).attr('data-img-src');
+          $('#capture_image').attr('src', src);
         });
       },
-      datatype: "json",
+      datatype: 'json',
     });
   };
 
